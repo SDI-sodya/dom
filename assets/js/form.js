7ef674c9c59e43a21d705690bff5085f293123f0
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-01-04 03:01:01 +0200

    Add advanced level of to-do list

--- assets/js/form.js
+++ assets/js/form.js
@@ -10,10 +10,12 @@ const addTask = document.querySelector("#toDoList");
 const tasks = document.querySelector("#tasks");
 
 let deleteTasksList = [];
+let todos = [];
 
 // addBtn.addEventListener("click", )
 addTask.addEventListener("submit", (e) => {
 	e.preventDefault();
+
 	const taskText = addTask.querySelector("#taskInput");
 	if (taskText.value !== "") {
 		const newTask = document.createElement("li");
@@ -21,14 +23,39 @@ addTask.addEventListener("submit", (e) => {
 		newTask.innerHTML = `<p class="taskText">${taskText.value}</p><button class="taskDltBtn">X</button>`;
 		tasks.appendChild(newTask);
 
-		// const newElem = new
-
+		const newElem = new Task(`${taskText.value}`);
+		todos.push(newElem);
 		deleteTasksList = document.querySelectorAll(".taskDltBtn");
+
 		deleteTasksList.forEach((btn) => {
 			btn.addEventListener("click", (e) => {
-				btn.parentElement.remove();
-				deleteTasksList = document.querySelectorAll(".taskDltBtn");
+				// debugger;
+
+				// Delete task from arr "todos"
+				let deletedTaskText =
+					btn.parentElement.querySelector(".taskText").textContent;
+				const deletedTaskIndex = todos
+					.map((el) => el.text)
+					.indexOf(deletedTaskText);
+				//
+				if (deletedTaskIndex !== -1) {
+					todos = todos
+						.slice(0, deletedTaskIndex)
+						.concat(todos.slice(deletedTaskIndex + 1));
+
+					// Delete task from UI
+					btn.parentElement.remove();
+					deleteTasksList = document.querySelectorAll(".taskDltBtn");
+				} else {
+					new TypeError("Unknown index of task");
+				}
 			});
 		});
 	}
 });
+
+// deleteTasksList.forEach((btn) => {
+// 	btn.addEventListener("click", (e) => {
+
+// 	});
+// });
